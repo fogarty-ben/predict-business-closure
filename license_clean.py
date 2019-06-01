ee671171db54d3f18c89f72756e904f8e273907d
Aya Liu <lzy@Aya-Lius-MacBook.local>
2019-06-01 17:08:09 -0500

    minor cleaning

--- license_clean.py
+++ license_clean.py
@@ -1,7 +1,6 @@
 import os
 import geopandas as gpd
 import pandas as pd
-import scipy
 from shapely.geometry import shape
 from dateutil import parser
 from datetime import timedelta
@@ -9,12 +8,7 @@ import pickle
 import json
 import shapely
 from sodapy import Socrata
-import missingno as msno
 import matplotlib.pyplot as plt
-
-import data_preprocess as prep
-import data_explore as exp
-import pipeline as pp
 
 pd.options.display.max_columns = 999
 
@@ -24,19 +18,7 @@ IDT = 'date_issued'
 ZILLOW_GEO = 'data/ZillowNeighborhoods-IL.shp'
 
 
-##### REPEATED FROM census_features.py
-def load_tokens(tokens_file):
-    '''
-    Loads dictionary of API tokens.
-
-    tokens_file (str): path to a JSON file containing API tokens
-    '''
-    with open(tokens_file,'r') as file:
-        return json.load(file)
-#####
-
-
-def get_lcs_data(pickle=True):
+def get_lcs_data():
 
     lcs = obtain_lcs()
     lcs = convert_lcs_dtypes(lcs)
@@ -160,240 +142,11 @@ def add_geography_id(gdf, b):
     return result
 
 
-# # sort data by license start date
-# lcs.sort_values(by=SDT, inplace=True)
-
-
-# # In[26]:
-
-
-# lcs.dtypes
-
-
-# # # Link data
-# # 
-# # to be done together
-
-# # # Train test split
-
-# # In[27]:
-
-
-# pipeline = pp.Pipeline()
-# pipeline.load_clean_data(lcs)
-
-
-# # In[28]:
-
-
-# start = parser.parse('1995-02-16') # earliest lic start date in original data
-# end = parser.parse('2020-07-16') # latest lic start date in original data
-# pipeline.get_train_test_times(start, end, 2, 2)
-
-# for i, (train_start, train_end, test_start, test_end) in enumerate(pipeline.train_test_times):
-#     print('N = {}'.format(i))
-#     print('TRAIN: START {} END {}'.format(train_start, train_end))
-#     print('TEST: START {} END {}\n'.format(test_start, test_end))
-
-
-# # # For 1 Split: (example)
-# # update split in pipeline
-
-# # In[64]:
-
-
-# # example
-# time_col = SDT
-# train_start, train_end, test_start, test_end = pipeline.train_test_times[3]
-
-
-# # In[65]:
-
-
-# train_df = lcs[(lcs[time_col] >= train_start) & (lcs[time_col] <= train_end)]
-# test_df = lcs[(lcs[time_col] >= test_start) & (lcs[time_col] <= test_end)]
-# lag_df = lcs[(lcs[time_col] > train_end) & (lcs[time_col] < test_start)]
-
-
-# # ## Before generating outcome: get latest license and other info
-# # 
-# # For each business:
-# # 1. Identify if business has applied for special type licenses in the 2-year period
-# # 2. Get the total number of licenses applied 
-# # 3. Get the latest license (latest start date)
-
-# # In[66]:
-
-
-# def get_special_apptypes_by_business(df):
-#     '''get dummies for whether business has applied for non-issuance/renewal licenses
-#     within the period'''
-#     app_type_dummies = pd.get_dummies(df['APPLICATION TYPE'], prefix='applied_for_type')
-#     other_app_type_dummies = app_type_dummies[['applied_for_type_C_CAPA', 
-#                                                'applied_for_type_C_EXPA',
-#                                                'applied_for_type_C_LOC']]
-#     temp = pd.concat([df, other_app_type_dummies], axis=1)
-#     other_type_by_bus = temp.groupby('business_id').agg(
-#                                     {'applied_for_type_C_CAPA': 'sum', 
-#                                      'applied_for_type_C_EXPA': 'sum',
-#                                      'applied_for_type_C_LOC': 'sum'}).reset_index()
-#     return other_type_by_bus
-
-
-# # In[67]:
-
-
-# def get_num_licenses_applied(df):
-#     return df.groupby('business_id').size().reset_index().rename(columns={0: 'num_licenses'})
-
-
-# # In[68]:
-
-
-# def get_latest_license(df):
-#     other_type_by_bus = get_special_apptypes_by_business(df)
-#     num_licenses = get_num_licenses_applied(df)
-#     latest = df[df['APPLICATION TYPE'].isin(['RENEW','ISSUE'])]                .groupby('business_id')                .tail(1)                .merge(other_type_by_bus, on='business_id')                .merge(num_licenses, on='business_id')
-#     return latest
-
-
-# # In[69]:
-
-
-# # get latest renew/issue license for each business
-# train_latest = get_latest_license(train_df)
-# test_latest = get_latest_license(test_df)
-
-
-# # ## Generate outcome
-
-# # In[75]:
-
-
-# def generate_label(latest, set_end, lag_df):
-    
-#     # init
-#     latest['closed'] = 0
-    
-#     # gen 1s
-#     for i, row in latest.iterrows():
-#         # latest license expires before train/test set end date -> closed
-#         if row[EDT] < set_end:
-#             latest.loc[i, 'closed'] = 1
-#         # latest license expires after end date, but not approved -> closed
-#         elif row['LICENSE STATUS'] != 'AAI':
-#             latest.loc[i, 'closed'] = 1
-#         # latest license expires after end date and are approved, but business did
-#         # not renew during lag period -> closed
-#         elif row['business_id'] in lag_df['business_id']:
-#             latest.loc[i, 'closed'] = 1
-
-
-# # In[76]:
-
-
-# # generate label for train
-# generate_label(train_latest, train_end, lag_df)
-
-
-# # In[78]:
-
-
-# # generate label for test
-# generate_label(test_latest, test_end, lag_df)
-
-
-# # In[77]:
-
-
-# train_latest.groupby('closed').size()
-
-
-# # In[79]:
-
-
-# test_latest.groupby('closed').size()
-
-
-# # ## Get X_train, y_train, X_test, y_test
-
-# # In[508]:
-
-
-# def split_Xy(train, test, predictor_cols, label):
-#     X_train = train[predictor_cols]
-#     y_train = train[label]
-#     X_test = test[predictor_cols]
-#     y_test = test[label]
-#     return X_train, y_train, X_test, y_test
-
-
-# # In[575]:
-
-
-# label = 'closed'
-# predictor_cols = ['ZIP CODE', 'WARD', 'PRECINCT', 'WARD PRECINCT', 
-#                   'POLICE DISTRICT', 'LICENSE CODE',
-#                   'APPLICATION TYPE', 'APPLICATION CREATED DATE', 
-#                   'APPLICATION REQUIREMENTS COMPLETE', 
-#                   'PAYMENT DATE', 'CONDITIONAL APPROVAL', 
-#                   'LICENSE TERM START DATE', 'LICENSE TERM EXPIRATION DATE', 
-#                   'LICENSE APPROVED FOR ISSUANCE', 'DATE ISSUED', 
-#                   'LICENSE STATUS', 'LICENSE STATUS CHANGE DATE', 
-#                   'SSA', 'LATITUDE', 'LONGITUDE', 'geometry', 'applied_for_type_C_CAPA', 
-#                   'applied_for_type_C_EXPA', 'applied_for_type_C_LOC', 'num_licenses']
-
-
-# # In[576]:
-
-
-# X_train, y_train, X_test, y_test = split_Xy(train_latest, test_latest, predictor_cols, label)
-
-
-# # # Feature Engineering
-
-# # ### 1. Application type
-# # no change
-
-# # ### 2. License Type
-
-# # In[577]:
-
-
-# # kept license types w/ > 1% counts
-# all_license_codes = X_train['LICENSE CODE'].value_counts(normalize=True).to_frame()
-# top_license_codes = all_license_codes[all_license_codes['LICENSE CODE'] > 0.01].index
-# prep.keep_top_values(lcs, 'LICENSE CODE', top_license_codes)
-
-
-# # In[578]:
-
-
-# top_license_codes
-
-
-# # In[579]:
-
-
-# lcs_raw[lcs_raw['LICENSE CODE'].isin(top_license_codes)].groupby(['LICENSE CODE', 'LICENSE DESCRIPTION']).size()
-
-
-# # ### 3. Number of Open Business in Locale (count of bus w/in two year periods w/in geography)
-
-# # In[584]:
-
-
-# X_train['ZIP CODE'].value_counts()
-
-
-# # In[528]:
-
-
-# lcs_raw.groupby('ZIP CODE').size()
-
-
-# # In[525]:
-
-
-# X_train.groupby('ZIP CODE').size()
-
+def load_tokens(tokens_file):
+    '''
+    Loads dictionary of API tokens.
+
+    tokens_file (str): path to a JSON file containing API tokens
+    '''
+    with open(tokens_file,'r') as file:
+        return json.load(file)
