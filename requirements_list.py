8f16ddf0c96b7783ac800e2a653c504fc9480a84
Ben Fogarty <ben.t.fogarty@gmail.com>
2019-06-04 21:53:28 -0500

    added requirements list

---
+++ requirements_list.py
@@ -0,0 +1,11 @@
+python==3.7.3
+numpy==1.16.2
+pandas==0.24.2
+matplotlib==3.0.3
+geopandas==0.5.0 (conda channel conda forge)
+shapely==1.6.4
+sodapy==1.5.2 (pip only)
+sklearn==0.20.3
+python-graphviz==0.10.1
+seaborn==0.9.0
+scipy==1.2.1
